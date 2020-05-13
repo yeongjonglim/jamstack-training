d6eb944f7ef801b980f43e5918c7829b012cfad8
voltendron <voltendron@gmail.com>
2020-05-13 22:18:47 +0800

    Integrated NetlifyCMS and Cloudinary

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('img');
     eleventyConfig.addPassthroughCopy('js');
     eleventyConfig.addPassthroughCopy('vendor');
+    eleventyConfig.addPassthroughCopy('admin');
 
     eleventyConfig.addFilter('dump', obj => {
         return util.inspect(obj)
